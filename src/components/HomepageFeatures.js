32c4a36067e793f97f0137733f1eccfa880002d4
Vamsi Annabathula <vamsi.annabathula@outlook.com>
2021-12-09 22:51:15 +0530

    added resume

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 const FeatureList = [
   {
-    title: 'About Me',
+    title: "About Me",
     //Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -15,11 +15,11 @@ const FeatureList = [
     ),
   },
   {
-    title: 'My Projects',
+    title: "My Projects",
     //Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Please visit my Github for now.
+        Please visit my Github.
         <a href="https://github.com/vamsi-annabathula"> Click here.</a>
         {/* Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
         ahead and move your docs into the <code>docs</code> directory. */}
@@ -27,7 +27,22 @@ const FeatureList = [
     ),
   },
   {
-    title: 'My Content',
+    title: "My Resume",
+    //Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        View/Download.
+        <a href="../../assets/myResume.pdf" target="_blank">
+          {" "}
+          Resume
+        </a>
+        {/* Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer. */}
+      </>
+    ),
+  },
+  {
+    title: "My Content",
     //Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
@@ -35,7 +50,10 @@ const FeatureList = [
         <a href="/blog"> Click here.</a>
         <br></br>
         My youtube Channel
-        <a href="https://www.youtube.com/channel/UCcCbCNG3yKxgtK7XjRhlIww"> Click here.</a>
+        <a href="https://www.youtube.com/channel/UCcCbCNG3yKxgtK7XjRhlIww">
+          {" "}
+          Click here.
+        </a>
         {/* Extend or customize your website layout by reusing React. Docusaurus can
         be extended while reusing the same header and footer. */}
       </>
@@ -43,11 +61,10 @@ const FeatureList = [
   },
 ];
 
-function Feature({
-  title, description }) {
+function Feature({ title, description }) {
   //Svg,
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--3")}>
       {/* <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div> */}
